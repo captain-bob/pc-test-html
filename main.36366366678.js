9ce26ab454d93a7f0ba9655f90b5c1de89e3188b
captain_bob <1083615314@qq.com>
2026-04-09 16:23:20 +0800

    sss

--- main.36366366678.js
+++ main.36366366678.js
@@ -2,7 +2,7 @@
     let p = document.createElement('p')
 
     // 2. 设置内容
-    p.innerText = '我是 JS 动态加进来的段落'
+    p.innerText = '我是 JS 动态加进来的段落56666'
 
     // 3. 加到页面里
     document.getElementById('container').appendChild(p)
